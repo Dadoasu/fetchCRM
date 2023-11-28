caae82a92508145d1ff826820aeed8c1f17b1aa2
Dadoasu <87278842+Dadoasu@users.noreply.github.com>
2023-11-28 07:45:07 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 
 const fetchURL = async () => {
   try {
-    const response = await axios.get('https://www.zohoapis.eu/crm/v2/functions/googleChromeCallBack/actions/execute?auth_type=apikey&zapikey=1003.29b5ebdbd0b1861c3c9fd73133b60dd7.a27648e944bcc78c7ba4076a51dc91e8&id=553068000009882122'); // Replace with your desired URL
+    const response = await axios.get('https://www.zohoapis.eu/crm/v2/functions/sdf/actions/execute?auth_type=apikey&zapikey=1003.9552b5269995cad6fca271a0f47923f5.66a0c058aea1351a19552c3ee8c67813'); // Replace with your desired URL
     console.log('Response:', response.data);
     // Handle the data or perform operations here
   } catch (error) {
